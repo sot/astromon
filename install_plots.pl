1723ccf44d463d83dcae67da263a00872f703979
Tom Aldcroft <taldcroft@gmail.com>
2011-08-30 17:08:08 -0400

    Increase plot sizes and fix a mistake in file installation directory

--- install_plots.pl
+++ install_plots.pl
@@ -15,14 +15,14 @@ GetOptions(\%opt,
 $Ska::Run::LOUD = 1;
 
 our $ASTROMON_DATA  = abs_path(dirname(__FILE__)) . "/data";
-our $ASTROMON_WWW   = abs_path(dirname(__FILE__)) . "/$opt{select_name}/www" ;
+our $ASTROMON_WWW   = abs_path(dirname(__FILE__)) . "/data/$opt{select_name}/www" ;
 
 chdir "$ASTROMON_DATA/$opt{select_name}";
 io($ASTROMON_WWW)->mkpath;
 
 foreach my $det (qw(ACIS-S ACIS-I HRC-S HRC-I)) {
-    run("ps2any -size 375 -rotate 90 " .
+    run("ps2any -size 475 -rotate 90 " .
 	"offsets-${det}.ps $ASTROMON_WWW/offsets-${det}.gif");
-    run("ps2any -size 300 -rotate 90  " .
+    run("ps2any -size 400 -rotate 90 " .
 	"offsets-${det}-hist.ps $ASTROMON_WWW/offsets-${det}-hist.gif");
 }
