a17a2103d5c810dcf35102b7882723b642f7618b
Javier Gonzalez <javierggt@yahoo.com>
2022-03-11 13:24:41 -0500

    update docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.dev109+g3648fc9.d20220112',
+    VERSION: '0.2.dev29+ge077a63',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
