eddedbe208868a38686f669581582d62bdec0614
Javier Gonzalez <javierggt@yahoo.com>
2022-10-19 12:03:56 -0400

    add --use-latest-calalign option to calc_astrometry_rms.py

--- astromon/scripts/calc_astrometry_rms.py
+++ astromon/scripts/calc_astrometry_rms.py
@@ -7,33 +7,55 @@ of 1.0 arcsec.
 
 import argparse
 import json
+import logging
 from pathlib import Path
 
 import numpy as np
 from astropy import units as u
 from cxotime import CxoTime
 
-from astromon import cross_match, db
+from astromon import cross_match, db, utils
 
 
 def get_parser():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--offsets-file", type=Path)
+    parser.add_argument("--use-latest-calalign", action="store_true", default=False)
+    parser.add_argument("--calalign-dir")
     return parser
 
 
 def main():
     args = get_parser().parse_args()
 
-    dat = db.get_cross_matches()
+    dat = db.get_cross_matches(
+        snr=5,
+        exclude_bad_targets=True,
+        sim_z=4,  # max sim-z
+        exclude_categories=[
+            "SN, SNR, and Isolated NS",
+            "Solar System and Misc",
+            "Clusters of Galaxies",
+        ],
+    )
     dat = dat[~cross_match.get_bad_target_mask(dat)]
+
+    no_version = dat["caldb_version"] == "0.0"
+    if np.any(no_version):
+        logging.getLogger("celmon").warning("Some observations with no version")
+        dat = dat[~no_version]
 
     select_name = np.unique(dat["select_name"])
     assert len(select_name) <= 1, f"More than one selection"
     assert len(dat), "no data"
     select_name = select_name[0]
 
-    if args.offsets_file:
+    if args.use_latest_calalign:
+        calalign = utils.get_calalign_offsets(dat, calalign_dir=args.calalign_dir)
+        dat["dy"] -= calalign["calalign_dy"] - calalign["ref_calalign_dy"]
+        dat["dz"] -= calalign["calalign_dz"] - calalign["ref_calalign_dz"]
+        dat["dr"] = np.sqrt(dat["dy"]**2 + dat["dz"]**2)
+    elif args.offsets_file:
         print(f"Subtracting offsets from {args.offsets_file}")
         with open(args.offsets_file) as fh:
             offsets = json.load(fh)
