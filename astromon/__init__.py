d74802bb598b218595fec4e9055f6ddc39686923
Javier Gonzalez <javierggt@yahoo.com>
2025-09-04 16:45:24 -0400

    set default log level for astromon logger

--- astromon/__init__.py
+++ astromon/__init__.py
@@ -4,3 +4,7 @@ __version__ = ska_helpers.get_version(__package__)
 
 from .cross_match import compute_cross_matches
 from .db import get_cross_matches
+
+logger = ska_helpers.logging.basic_logger(
+    "astromon", level="CRITICAL", format="%(asctime)s %(funcName)-25s: %(message)s"
+)
