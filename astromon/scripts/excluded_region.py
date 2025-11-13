5b037f40e2d4484f60d51e036ea52284529214c4
Javier Gonzalez <javierggt@yahoo.com>
2025-11-13 15:56:28 -0500

    added sync option to astromon-excluded-region script

--- astromon/scripts/excluded_region.py
+++ astromon/scripts/excluded_region.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 """
-Script to add excluded regions to astromon.db
+Script to manipulate excluded regions to astromon.db
 """
 
 import argparse
 import getpass
 import logging
+import shutil
+from pathlib import Path
 
 from astropy.coordinates import SkyCoord
 from ska_helpers.logging import basic_logger
@@ -24,7 +26,67 @@ def get_parser():
     config_levels += [x.lower() for x in config_levels]
 
     parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        add_help=False,
+    )
+    parser.add_argument(
+        "action", choices=list(ACTIONS), help="Action to perform", nargs="?"
+    )
+    parser.add_argument(
+        "-h",
+        "--help",
+        default=False,
+        action="store_true",
+        help="Show this help message and exit",
+    )
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        choices=config_levels,
+        default="INFO",
+    )
+    return parser
+
+
+def get_sync_parser():
+    config_levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    config_levels += [x.lower() for x in config_levels]
+
+    parser = argparse.ArgumentParser(
         description="Add excluded regions to astromon.db",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "action",
+        choices=list(ACTIONS),
+        help="Action to perform",
+    )
+    parser.add_argument(
+        "from_file",
+        type=Path,
+        help="Source astromon DB file",
+    )
+    parser.add_argument(
+        "to_file",
+        type=Path,
+        help="Destination astromon DB file",
+    )
+    parser.add_argument(
+        "--log-level",
+        type=str,
+        choices=config_levels,
+        default="INFO",
+    )
+    return parser
+
+
+def get_actions_parser():
+    config_levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    config_levels += [x.lower() for x in config_levels]
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
@@ -78,8 +140,12 @@ def get_parser():
 
 def main(args=None):
     parser = get_parser()
-    args = parser.parse_args(args)
+    args, _ = parser.parse_known_args(args)
     args.log_level = args.log_level.upper()
+
+    if args.help and not args.action:
+        parser.print_help()
+        return
 
     logger.setLevel(args.log_level)
     # this downgrades astromon's INFO to WARNING to avoid too much output
@@ -87,8 +153,11 @@ def main(args=None):
         args.log_level if args.log_level != "INFO" else "WARNING"
     )
 
+    action_parser = PARSERS[args.action]()
+    action_args = action_parser.parse_args()
+
     try:
-        ACTIONS[args.action](args)
+        ACTIONS[args.action](action_args)
     except ArgumentError as e:
         parser.error(str(e))
     except Exception as e:
@@ -144,6 +213,17 @@ def list_regions(args):
     regions.pprint(max_width=-1, max_lines=-1)
 
 
+def sync_regions(args):
+    from astromon.db import sync_regions
+
+    if not args.from_file.exists():
+        raise ArgumentError(f"Source file {args.from_file} does not exist")
+    if not args.to_file.exists():
+        shutil.copy(args.from_file, args.to_file)
+
+    sync_regions(args.from_file, args.to_file)
+
+
 OUT = """Added region:
     ID:       {region_id}
     RA:       {ra}
@@ -159,6 +239,15 @@ ACTIONS = {
     "remove": remove_region,
     "rm": remove_region,
     "list": list_regions,
+    "sync": sync_regions,
+}
+
+PARSERS = {
+    "add": get_actions_parser,
+    "remove": get_actions_parser,
+    "rm": get_actions_parser,
+    "list": get_actions_parser,
+    "sync": get_sync_parser,
 }
 
 if __name__ == "__main__":
